4fa5a07c3d65475b613f7afbb8fd74a2fa22db13
andrzejkozio <akoziol@ch.pw.edu.pl>
2016-12-11 13:47:24 +0100

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -45,10 +45,24 @@ public class Main {
         stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
         stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
         ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+ArrayList<String> output = new ArrayList<String>();
+        while (rs.next()) {
+          output.add( "Read from DB: " + rs.getTimestamp("tick"));
+        }
 
+        attributes.put("results", output);
+        return new ModelAndView(attributes, "db.ftl");
+      } catch (Exception e) {
+        attributes.put("message", "There was an error: " + e);
+        return new ModelAndView(attributes, "error.ftl");
+      } finally {
+        if (connection != null) try{connection.close();} catch(SQLException e){}
+      }
         
     }, new FreeMarkerEngine());
 
- 
+    }
+        
+        }
 
         
